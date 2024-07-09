712f11b19e836fc7f1b061c87e89e4ac64687f90
Rajan <rajanj448@student.sfit.ac.in>
2024-07-09 12:17:32 +0530

    Choices component

--- components/Choices.tsx
+++ components/Choices.tsx
@@ -1,4 +1,6 @@
 import { Choice } from '@/lib/types'
+import { RadioGroup } from "@headlessui/react";
+import clsx from 'clsx';
 import React from 'react'
 
 
@@ -8,9 +10,29 @@ interface ChoicesProps{
     setValue:(_:Choice)=>void
 }
 
-const Choices = () => {
+const Choices = ({choices,initialValue,setValue}:ChoicesProps) => {
   return (
-    <div>Choices</div>
+    <RadioGroup value={initialValue} onChange={setValue}>
+       <div className="flex gap-3 py-[7px] text-sm">
+        {choices.map((choice) => (
+          <RadioGroup.Option
+            key={choice.label}
+            value={choice}
+            className={clsx("cursor-pointer select-none rounded-md")}
+          >
+            <span
+              className={clsx(
+                "rounded-md py-1 px-2",
+                "transition-colors duration-100 ease-in-out"
+              )}
+            >
+              {choice.label}
+            </span>
+          </RadioGroup.Option>
+        ))}
+      </div>
+
+    </RadioGroup>
   )
 }
 
